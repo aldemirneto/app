7a8bcd6aa150b0c5c524d3f5d8c891b5d1f06f53
Aldemir.neto <humbertoaldemir@gmail.com>
2021-11-16 21:08:27 -0300

    é isso

---
+++ app/src/main/java/br/edu/ifspprc/teste/SegundaTela.kt
@@ -0,0 +1,11 @@
+package br.edu.ifspprc.teste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SegundaTela : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda_tela)
+    }
+}
